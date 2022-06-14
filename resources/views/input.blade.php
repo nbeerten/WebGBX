577436d7e7e9cd893236475a40723199657a89eb
nbeerten <59444993+nbeerten@users.noreply.github.com>
2022-06-14 16:35:35 +0200

    First bits of setup

---
+++ resources/views/input.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>WebGBX</title>
+    </head>
+    <body>
+        <div>
+            <h1>WebGBX</h1>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\parseGBX;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('input');
 });
+
+Route::get('/gbx', [parseGBX::class, 'index']);
